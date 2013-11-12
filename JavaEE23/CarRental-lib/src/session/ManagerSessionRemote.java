edcb3c9eb3e294cd5bb816c869e29133a08872c5
Stijn Hoskens <stijn.hoskens@student.kuleuven.be>
2013-11-12 17:30:10 +0100

    lib commit

--- JavaEE23/CarRental-lib/src/session/ManagerSessionRemote.java
+++ JavaEE23/CarRental-lib/src/session/ManagerSessionRemote.java
@@ -1,12 +1,20 @@
 package session;
 
+import java.util.List;
 import java.util.Set;
 import javax.ejb.Remote;
+import rental.Car;
 import rental.CarType;
-import rental.Reservation;
+import rental.ReservationException;
 
 @Remote
 public interface ManagerSessionRemote {
+    
+    public void addCar(String companyName, Car car) throws ReservationException;
+    
+    public void addCarType(String companyName, CarType type) throws ReservationException;
+    
+    public void addCompany(String companyName, List<Car> cars);
     
     public Set<CarType> getCarTypes(String company);
     
